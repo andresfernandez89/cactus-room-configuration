f4c61218d57ba8bc3879e1c9f26395f82cb87e4d
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-09-25 12:46:53 -0300

    refactor: Refactor point drawing logic

--- src/app/components/RoomConfigurator.tsx
+++ src/app/components/RoomConfigurator.tsx
@@ -1,70 +1,94 @@
 "use client";
-import { useEffect, useRef } from "react";
-import usePoints from "../hooks/usePoints";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
-const RoomConfigurator: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const points = usePoints();
+const RoomConfigurator = () => {
+  const points = [
+    {
+      id: "EnRd7hAaNydVdVJ06qgF",
+      coordX: 40,
+      coordY: 86,
+      name: "Pavimento",
+    },
+    {
+      id: "Ks5CthbPwAvd2TNxzHEl",
+      coordX: 71,
+      coordY: 38,
+      name: "Frente",
+    },
+    {
+      id: "cd84QwP9gOhAU5p47UDn",
+      coordX: 52,
+      coordY: 55,
+      name: "Encimera",
+    },
+    {
+      id: "i7EVutewtycZY2qwmldG",
+      coordX: 61,
+      coordY: 47,
+      name: "Entrepaños",
+    },
+  ];
+
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const [imageDimensions, setImageDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const updateImageDimensions = () => {
+    if (imageRef.current) {
+      const { width, height } = imageRef.current.getBoundingClientRect();
+      setImageDimensions({ width, height });
+    }
+  };
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      const context = canvas.getContext("2d");
-      if (context instanceof CanvasRenderingContext2D) {
-        const baseImage = new window.Image();
-        baseImage.src = "/images/base.webp";
-        const svgImage = new window.Image();
-        svgImage.src = "/icons/fingerprintIcon.svg";
+    updateImageDimensions();
 
-        baseImage.onload = () => {
-          // base image
-          context.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+    window.addEventListener("resize", updateImageDimensions);
 
-          svgImage.onload = () => {
-            points.forEach((point) => {
-              const coordX = (point.coordX * canvas.width) / 100;
-              const coordY = (point.coordY * canvas.height) / 100;
-
-              // outer ring
-              context.globalAlpha = 0.6;
-              context.beginPath();
-              context.arc(coordX + 15, coordY + 15, 28, 0, 2 * Math.PI);
-              context.fillStyle = "#333";
-              context.fill();
-
-              // white ring
-              context.globalAlpha = 0.6;
-              context.beginPath();
-              context.arc(coordX + 15, coordY + 15, 25, 0, 2 * Math.PI);
-              context.lineWidth = 2;
-              context.strokeStyle = "white";
-              context.stroke();
-
-              // Inner circle
-              context.globalAlpha = 0.2;
-              context.beginPath();
-              context.arc(coordX + 15, coordY + 15, 20, 0, 2 * Math.PI);
-              context.fillStyle = "#94a3b8";
-              context.fill();
-              context.globalAlpha = 0.6;
-
-              //svg fingerprint
-              context.drawImage(svgImage, coordX, coordY, 30, 30);
-            });
-          };
-        };
-      }
-    }
-  }, [points]);
+    return () => {
+      window.removeEventListener("resize", updateImageDimensions);
+    };
+  }, []);
 
   return (
     <div className="w-full max-md:max-w-md md:max-w-6xl mx-auto">
-      <canvas
-        ref={canvasRef}
-        className="w-full h-auto border"
-        width={1240}
-        height={873}
-      />
+      <div className="relative">
+        <Image
+          ref={imageRef}
+          src="/images/base.webp"
+          alt="Room"
+          className="relative w-full h-auto border"
+          width={1240}
+          height={873}
+          priority
+        />
+
+        {points.map((point) => (
+          <div
+            key={point.id}
+            className="absolute"
+            style={{
+              top: (point.coordY * imageDimensions.height) / 100,
+              left: (point.coordX * imageDimensions.width) / 100,
+              zIndex: 10,
+            }}
+            onClick={() => {
+              setSelectedPoint(point.id);
+            }}
+          >
+            <Image
+              src="/icons/fingerprintIcon.svg"
+              alt="Fingerprint Icon"
+              width={20}
+              height={20}
+              className="p-1 cursor-pointer border-2 bg-[#475569] border-[#475569] opacity-50 rounded-full sm:h-[25px] sm:w-[25px] md:h-[40px] md:w-[40px] ring-2 ring-white ring-inset" // Para mostrar el cursor como puntero al pasar por encima
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
